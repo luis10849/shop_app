93b968fdce500ce596a1240e9e5fcef0c4c55044
luis osorio gonzalez <luisjavierosorio3@gmail.com>
2020-12-30 12:26:18 -0500

    init

---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+
+  static String routeName = "/login";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+    );
+  }
+}
